3f9f5c887ac020460a31529d35bc82517f5c3826
Me <getdaniel>
2023-01-11 11:10:38 +0300

    old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Ranks country origins of bands, ordered by number of fans.
+SELECT band_name, (IFNULL(split, '2023') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
