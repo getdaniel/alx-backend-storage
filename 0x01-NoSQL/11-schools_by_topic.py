8c7ea72ba6a8a2b94bbe692d1ebec70eba7232ff
Me <getdaniel>
2023-01-18 00:20:29 +0300

    where can I learn python

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Learn Python."""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic."""
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {'$eq': topic},
+            },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
