42b5db815089d5894a93cb5427a6f2e990b504d9
Me <getdaniel>
2023-01-18 22:51:41 +0300

    final commit for today

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,19 +3,19 @@
     obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-r = redis.Redis()
+
+
+redi = redis.Redis()
 count = 0
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
+    """ Impelements core function of get page which is accessed."""
+    redi.set(f"cached:{url}", count)
+    response = requests.get(url)
+    redi.incr(f"count:{url}")
+    redi.setex(f"cached:{url}", 10, redi.get(f"cached:{url}"))
+    return response.text
 
 
 if __name__ == "__main__":
