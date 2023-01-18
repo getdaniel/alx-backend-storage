81ad669b9553f7fd09d5fbc056f4d1aa59e1874d
Me <getdaniel>
2023-01-18 11:31:12 +0300

    increamenting redis values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """ Redis Basic."""
+from functools import wraps
 import redis
-from typing import Callable, Union
+from typing import Any, Callable, Union
 from uuid import uuid4
+
+
+def count_calls(method: Callable) -> Callable:
+    """Implement a system to count how many times methods
+       of the Cache class are called.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kargs) -> Any:
+        """ Implements counting functionality."""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kargs)
+
+    return wrapper
 
 
 class Cache:
@@ -12,6 +27,7 @@ class Cache:
         self._redis =  redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key (e.g. using uuid), store the input data
            in Redis using the random key and return the key.
